b535bacfb48ee56b4ffa989edf2c000b24049e8c
largomst <dingrui.zhen@gmail.com>
2019-11-21 21:55:25 +0800

    add hello world

---
+++ main.c
@@ -0,0 +1,13 @@
+// 各种工具的使用都不是编程语言的本质，但是记住这些工具的使用是我们的最终目的，结果上也唤醒了我们对编程语言本质的记忆。
+
+#include <stdio.h>
+
+#define LOWER 0
+#define HIGH 10
+
+int main(){
+    printf("Hello World");
+    return 0;
+}
+
+
